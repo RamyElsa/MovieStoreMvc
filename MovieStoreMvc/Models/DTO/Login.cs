e119521a2f14de4c52590e13e2b47a1e1c140e78
RamyElsa <ramye301@gmail.com>
2023-01-24 15:09:25 +0200

    first commit

---
+++ MovieStoreMvc/Models/DTO/Login.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieStoreMvc.Models.DTO
+{
+    public class Login
+    {
+        [Required]
+        public string? Username { get; set; }
+
+        [Required]
+        public string? Password { get; set; }
+
+    }
+}
+
---
+++ MovieStoreMvc/Repositories/Abstract/IUserAuthenticationService.cs
@@ -0,0 +1,13 @@
+﻿using MovieStoreMvc.Models.DTO;
+
+namespace MovieStoreMvc.Repositories.Abstract
+{
+    public interface IUserAuthenticationService
+    {
+
+        Task<Status> LoginAsync(Login model);
+        Task LogoutAsync();
+        Task<Status> RegisterAsync(Registration model);
+        //Task<Status> ChangePasswordAsync(ChangePasswordModel model, string username);
+    }
+}
